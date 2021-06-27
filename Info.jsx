07d76585acbe81131172ed73881de63f967f3bba
Rajeshwari-M <86568102+Rajeshwari-M@users.noreply.github.com>
2021-06-27 23:10:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+  <h1>JavaScript and React.js</h1>
+  <p>bootcamp on JavaScript and React.js starts on 27th june</p>
+      </div>
+  );
+}
+export default Info;
